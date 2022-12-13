6058dff7eec50106ef4415e39864f6ee936fef1e
marvin espira <espiramarvin@gmail.com>
2022-12-13 18:52:22 +0300

    feat: adds updates

---
+++ utils/location.js
@@ -0,0 +1,10 @@
+import { GOOGLE_MAPS_API_KEY } from "@env"
+
+export function getMapPreview(lat, lng) {
+  console.log("lat", lat)
+  console.log("lng", lng)
+
+  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_MAPS_API_KEY}`
+
+  return imagePreviewUrl
+}
