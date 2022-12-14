a605c37d19363ce9fc3333778bb5cf3bab8ebba9
marvin espira <espiramarvin@gmail.com>
2022-12-14 15:27:57 +0300

    adds updates

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { useEffect, useState } from "react"
+import { StyleSheet } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import AppNavigation from "./navigation/AppNavigation"
+import * as SplashScreen from "expo-splash-screen"
+import { init } from "./utils/database"
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false)
+  useEffect(() => {
+    // initialize sqlite db
+    init()
+      .then(() => {
+        setAppIsReady(true)
+      })
+      .catch((err) => console.log("err", err))
+  }, [appIsReady])
+
+  if (!appIsReady) {
+    return null
+  } else {
+    SplashScreen.hideAsync()
+  }
+
   return (
     <>
       <StatusBar style="light" backgroundColor="#000000" />
