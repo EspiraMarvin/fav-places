5ee61a9d9b2d142b65187a33c385143e923828c4
marvin espira <espiramarvin@gmail.com>
2022-12-14 12:05:56 +0300

    feat: adds updates

---
+++ components/ui/Button.js
@@ -0,0 +1,38 @@
+import { View, Text, Pressable, StyleSheet } from "react-native"
+import React from "react"
+import { Colors } from "../../constants/colors"
+
+export default function Button({ onPress, children }) {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{children} </Text>
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    margin: 4,
+    backgroundColor: Colors.primary700,
+    elevation: 2,
+    shadowColor: "black",
+    shadowOpacity: 0.15,
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 2,
+    borderRadius: 4,
+    marginTop: 16,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 16,
+    color: Colors.primary50,
+  },
+})
